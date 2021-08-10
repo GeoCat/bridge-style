2ac8fd4d05fd4d82316972d8278afe2b9460ed4f
Sander Schaminee <sander.schaminee@geocat.net>
2021-08-10 13:03:22 +0200

    Updated and fixed setup (subpackages were not included)

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="bridgestyle",
     version="0.1",
-    author="GeoCat",
+    author="GeoCat BV",
     author_email="volaya@geocat.net",
-    description="A library to convert between different map style formats",
+    description="A Python library to convert between different map style formats",
     license="MIT",
-    keywords="GeoCat",
-    url="",
-    packages=["bridgestyle"],
+    keywords=["GeoCat", "Bridge", "style", "symbology", "styling", "mapping", "SLD", "cartography",
+              "Mapfile", "CIM", "GeoStyler", "Mapbox", "QGIS", "GeoServer", "Esri"],
+    url="https://github.com/GeoCat/bridge-style",
+    packages=find_packages(),
     entry_points={"console_scripts": ["style2style=bridgestyle.style2style:main"]},
 )
