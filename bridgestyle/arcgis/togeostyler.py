ef71ef06ece839272d7be235b370b66165134806
Cécile Vuilleumier <cecile.vuilleumier@camptocamp.com>
2022-09-23 13:15:22 +0200

    Add support for GraduatedSymbol (#19)

--- bridgestyle/arcgis/togeostyler.py
+++ bridgestyle/arcgis/togeostyler.py
@@ -50,7 +50,7 @@ def processLayer(layer, options=None):
 
         elif (
             renderer["type"] == "CIMClassBreaksRenderer"
-            and renderer.get("classBreakType") == "GraduatedColor"
+            and renderer.get("classBreakType") in ["GraduatedColor", "GraduatedSymbol"]
         ):
             rules.extend(processClassBreaksRenderer(renderer, options))
         else:
