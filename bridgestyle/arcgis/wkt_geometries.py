fd00d259423843552b0c12f1b796cb3e13bf3c32
Cécile Vuilleumier <cecile.vuilleumier@camptocamp.com>
2023-03-01 14:31:21 +0100

    Fix size and spacing in polygon fill (#40)

--- bridgestyle/arcgis/wkt_geometries.py
+++ bridgestyle/arcgis/wkt_geometries.py
@@ -1,11 +1,16 @@
+from .constants import PT_TO_PX_FACTOR
+
+def pt_to_px(pt):
+    return round(pt * PT_TO_PX_FACTOR)
+
 def to_wkt(geometry):
     if geometry.get("rings"):
         rings = geometry["rings"][0]
-        coordinates = ", ".join([" ".join([str(i) for i in j]) for j in rings])
+        coordinates = ", ".join([" ".join([str(pt_to_px(i)) for i in j]) for j in rings])
         return {
                     "wellKnownName": f"wkt://POLYGON(({coordinates}))",
-                    "maxX": max([coord[0] for coord in rings]),
-                    "maxY": max([coord[1] for coord in rings]),
+                    "maxX": pt_to_px(max([coord[0] for coord in rings])),
+                    "maxY": pt_to_px(max([coord[1] for coord in rings])),
         }
     # The following corresponds to the geometry of the line symbolizer in ArcGIS
     elif geometry.get("paths") and geometry["paths"][0] == [[2, 0], [-2, 0]]:
