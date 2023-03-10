0bcb19431f59931d0eaa4111de4338c2b6763c04
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2023-03-10 10:30:12 +0100

    Keep geometry rings in default unit size

--- bridgestyle/arcgis/wkt_geometries.py
+++ bridgestyle/arcgis/wkt_geometries.py
@@ -1,13 +1,11 @@
-from .constants import pt_to_px
-
 def to_wkt(geometry):
     if geometry.get("rings"):
         rings = geometry["rings"][0]
-        coordinates = ", ".join([" ".join([str(pt_to_px(i)) for i in j]) for j in rings])
+        coordinates = ", ".join([" ".join([str(i) for i in j]) for j in rings])
         return {
                     "wellKnownName": f"wkt://POLYGON(({coordinates}))",
-                    "maxX": pt_to_px(max([coord[0] for coord in rings])),
-                    "maxY": pt_to_px(max([coord[1] for coord in rings])),
+                    "maxX": max([coord[0] for coord in rings]),
+                    "maxY": max([coord[1] for coord in rings]),
         }
     # The following corresponds to the geometry of the line symbolizer in ArcGIS
     elif geometry.get("paths") and geometry["paths"][0] == [[2, 0], [-2, 0]]:
